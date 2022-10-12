2beebfc428fbb8038e82da235a4cae7717027114
pendi1901 <pendyala20096@iiitd.ac.in>
2022-10-13 02:53:30 +0530

    Update: Adding Threads and Answers

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,48 @@ app.get('/questions/:id',( request,res   )=>{
 
 })
 
+//Post a new question by a user
+app.post('/questions/:id',( request,res   )=>{
+    const threadID = parseInt(request.params.id)
+    const {subject} = request.body.subject
+    const {content}  =request.body.content
+
+    pool.query('INSERT INTO Threads (threadID,subject,content,upvotes,reportCount,isBanned,isPosted,createdAt,updatedAt) VALUES ($1,$2,$3)', [threadID,subject,content,0,0,0,Date,Date], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+
+    })
+})
+
+//Post a new answer to a question
+app.post('/answers/:id',( request,res   )=>{
+    const threadID = parseInt(request.params.id)
+    const{subject}=request.subject
+    const {content}  =request.content
+
+    pool.query('INSERT INTO Answers (answerID,subject,content,reportCount,upvotes,isApproved,isBanned,isPosted,createdAt,updatedAt) VALUES ($1,$2,$3)', [threadID,subject,content,0,0,0,0,Date,Date], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+
+    })
+})
+
+//Get all answers to a question
+app.get('/thread/answers/:id',( request,res   )=>{
+    const threadID = request.body.threadID
+    // const answerID = parseInt(request.params.id)
+    pool.query('SELECT * FROM Answer WHERE threadID = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+
+    })
+})
 
 
 app.listen(port, () => {
