4f3bdf3b38ce566aee53a52533d96c4b38c75b0d
pendi1901 <pendyala20096@iiitd.ac.in>
2022-10-13 02:13:35 +0530

    Update: User profile data

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000
 
-//connect using postgres
+// connect using postgres
 const { Pool } = require('pg');
+const {response} = require("express");
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -26,6 +27,33 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+//Get  User Profile
+app.get('/user/:id',( request,res   )=>{
+
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+
+    })
+
+})
+
+//Get All Questions from user
+app.get('/questions/:id',( request,res   )=>{
+
+        const id = parseInt(request.params.id)
+        pool.query('SELECT * FROM questions WHERE id = $1', [id], (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).json(results.rows)
+
+        })
+
+})
 
 
 
