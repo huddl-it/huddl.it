5c756e1583affb3820cd2d18415560fd713c1538
Jaideep <guntupallijaideep@gmail.com>
2022-10-13 02:49:33 +0530

    Feat: Added Posts to Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,10 +7,48 @@ function App() {
 
     return (
         <Layout>
-            <h1 className="text-7xl font-dm">Hi Mom!</h1>
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
+            <div className="flex flex-col gap-6">
+                <div className="bg-white/70 backdrop-blur-xl px-12 py-8 rounded-xl max-w-3xl cursor-pointer">
+                    <h2 className="text-xl font-bold mb-2 hover:underline">
+                        How to be cool like Ritvik Pendyala?
+                    </h2>
+                    <p>
+                        I have seen Ritvik Pendyala in and around the campus. I
+                        want to be as cool as him. Please someone give tips to
+                        become like Ritvik sir. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Quo sint saepe ut tenetur
+                        adipisci ipsam officiis, deserunt harum pariatur autem
+                        nobis voluptas quod dignissimos aut nihil laborum
+                        fugiat, magnam earum. I have seen Ritvik Pendyala in and
+                        around the campus. I want to be as cool as him. Please
+                        someone give tips to become like Ritvik sir. Lorem ipsum
+                        dolor sit amet consectetur adipisicing elit. Quo sint
+                        saepe ut tenetur adipisci ipsam officiis, deserunt harum
+                        pariatur autem nobis voluptas quod dignissimos aut nihil
+                        laborum fugiat, magnam earum.
+                    </p>
+                </div>
+                <div className="bg-white/70 backdrop-blur-xl px-12 py-8 rounded-xl max-w-3xl cursor-pointer">
+                    <h2 className="text-xl font-bold mb-2 hover:underline">
+                        How to be cool like Tejdeep Chippa?
+                    </h2>
+                    <p>
+                        I have seen Tejdeep Chippa in and around the campus. I
+                        want to be as productive as him. Please someone give
+                        tips to become such businessman like Chippa sir. Lorem
+                        ipsum dolor sit amet consectetur adipisicing elit. Quo
+                        sint saepe ut tenetur adipisci ipsam officiis, deserunt
+                        harum pariatur autem nobis voluptas quod dignissimos aut
+                        nihil laborum fugiat, magnam earum. I have seen Ritvik
+                        Pendyala in and around the campus. I want to be as cool
+                        as him. Please someone give tips to become like Ritvik
+                        sir. Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Quo sint saepe ut tenetur adipisci ipsam officiis,
+                        deserunt harum pariatur autem nobis voluptas quod
+                        dignissimos aut nihil laborum fugiat, magnam earum.
+                    </p>
+                </div>
+            </div>
         </Layout>
     );
 }
